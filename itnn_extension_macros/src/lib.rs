c0bfe6dc71a50f2dc0ab126b47a1964b789c5ffc
Jameson <james.bradlee@telenor.no>
2023-10-28 13:00:47 +0200

    Added some documentation

--- itnn_extension_macros/src/lib.rs
+++ itnn_extension_macros/src/lib.rs
@@ -12,6 +12,21 @@ pub fn op(args: TokenStream, input: TokenStream) -> TokenStream {
 	op::expand(syn::parse2::<syn::Item>(input.into()).expect("could not get syntax tree of item")).into()
 }
 
+/// A helper macro to define an extension.
+///
+/// Properties:
+/// - `name`? ([str]) - Optionally define a custom name for your extension, defaults to the package name.
+/// - `version`? ([str]) - Optionally define a custom version for your extension, defaults to the package versions.
+/// - `dependencies`? (map `<dependency name> = [optional] "<dependency version req>"`), defaults to no dependencies.
+///     ```
+///       tnn::extension!(
+///         dependencies = {
+///           tnn = "^0.1",
+///           my_cool_extension = optional "1.2.3"
+///         }
+///       )
+///     ```
+/// - `ops`? (`[path::to::op_function]`), defaults to no operations.
 #[proc_macro]
 pub fn extension(input: TokenStream) -> TokenStream {
 	match extension::parse(input.into()) {
